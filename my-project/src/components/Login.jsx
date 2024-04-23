9a02d41957782f2e8ea4ef24c2345911e6cde1b0
almeh7ri <ahmedalmehri27@gmail.com>
2024-04-23 19:05:33 -0400

    changed the login page design

--- my-project/src/components/Login.jsx
+++ my-project/src/components/Login.jsx
@@ -1,18 +1,18 @@
 // src/components/Login.jsx
-
 import React from "react";
+import './Login.css'; // Import the CSS file for styles
 
 const LoginPage = () => {
   const handleGoogleLogin = () => {
-    // Redirect user to Google login, this one needs to also be on the google cloud console
+    // Redirect user to Google login, this one needs to also be on the Google Cloud Console
     window.location.href = "http://localhost:5178/api/auth/google";
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="login-container">
+      <h1 className="title">Current Events Dashboard</h1>
       {/* Button to handle login via Google OAuth */}
-      <button onClick={handleGoogleLogin}>Login with Google</button>
+      <button onClick={handleGoogleLogin} className="login-button">Login with Google</button>
     </div>
   );
 };
